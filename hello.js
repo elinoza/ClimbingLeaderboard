ff26efaccf6ae511d217525774b8119ecd93ab63
elinoza <hillcakmak@gmail.com>
2022-12-10 19:12:58 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,17 +1,31 @@
 function climbingLeaderboard(ranked, player) {
   // Write your code here
-let rank=[];
-let compareNumbers=(a, b) => {
-return b-a;
-}
-let makeUnique = (ranked)=>{return [...new Set(ranked)] }
-player.map((play)=> {
- let count=0;
-   ranked.push(play)
- let filtered= makeUnique(ranked) 
- filtered.sort(compareNumbers)
- let index=filtered.findIndex(elem=>elem===play)
-  rank.push(index+1)
+player=player.reverse()
+let index=0;
+let count=0;
+let ranks=[];
+
+
+console.log("ranked:",ranked,"player:",player)
+player.map(score=> {
+   for(let i=count; i<ranked.length; i++)
+   {
+       console.log("score",score,"ranked[i]",ranked[i],"i",i,"ranks",ranks,"index",index)
+       if(score<ranked[i] && ranked[i]=== ranked[i-1]){index=index-1};
+      if(score<ranked[i] && i===(ranked.length-1)){ranks.push(index+2);
+      //console.log("hey") 
+      }
+      else if( score >= ranked[i] ){ count=i;ranks.push(index+1);break}
+   index++
+    }
 })
-return rank
+//console.log(ranks,index)
+return ranks.reverse()
+
+
+
+
+
+
+
 }
