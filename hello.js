9c7589b6ebdfeaaadea069c2736db5f837ad11bb
elinoza <hillcakmak@gmail.com>
2022-12-11 00:41:43 +0300

    Update hello.js
    it is basically the same code as I did before but time limit doesnt exceed. probably it is about time complexity ... Mine was map and for let iteration together...

--- hello.js
+++ hello.js
@@ -1,31 +1,33 @@
 function climbingLeaderboard(ranked, player) {
   // Write your code here
 player=player.reverse()
-let index=0;
-let count=0;
-let ranks=[];
+const ranks=[];
+let lastScore=null;
+let lastRank=1;
+
+let currentScoreIndex=0;
 
 
-console.log("ranked:",ranked,"player:",player)
-player.map(score=> {
-   for(let i=count; i<ranked.length; i++)
-   {
-       console.log("score",score,"ranked[i]",ranked[i],"i",i,"ranks",ranks,"index",index)
-       if(score<ranked[i] && ranked[i]=== ranked[i-1]){index=index-1};
-      if(score<ranked[i] && i===(ranked.length-1)){ranks.push(index+2);
-      //console.log("hey") 
-      }
-      else if( score >= ranked[i] ){ count=i;ranks.push(index+1);break}
-   index++
-    }
+
+
+//console.log("ranked:",ranked,"player:",player)
+player.map(aliceValue=> {
+  while(currentScoreIndex < ranked.length)
+  {
+    let scoreValue= ranked[currentScoreIndex];
+   // console.log("aliceValue",aliceValue,"ranked[currentScoreIndex]",ranked[currentScoreIndex],"currentScoreIndex",currentScoreIndex,"lastRank",lastRank,"currentScoreIndex",currentScoreIndex)
+  if(aliceValue>= scoreValue){ranks.push(lastRank);break}
+  if(scoreValue!== lastScore){lastRank+=1;
+  lastScore=scoreValue}
+  currentScoreIndex+=1
+  }
+  if(currentScoreIndex>=ranked.length){ranks.push(lastRank)}
 })
 //console.log(ranks,index)
 return ranks.reverse()
 
-
-
-
-
-
-
 }
+
+let ranked=[100,100,50,40,40,20,10];
+let player=[120,50,25,5]
+climbingLeaderboard(ranked,player)
